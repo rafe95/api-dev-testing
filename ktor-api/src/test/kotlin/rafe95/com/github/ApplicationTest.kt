12c9c1432aecc086679ff6c5b8746025a21a9abb
Rafael Estevez <rafe.estevez@gmail.com>
2022-04-18 00:58:50 -0400

    add: test to order routes

--- ktor-api/src/test/kotlin/rafe95/com/github/ApplicationTest.kt
+++ ktor-api/src/test/kotlin/rafe95/com/github/ApplicationTest.kt
@@ -1,27 +1,26 @@
 package rafe95.com.github
 
-import io.ktor.server.routing.*
-import io.ktor.http.*
-import io.ktor.server.plugins.*
-import io.ktor.serialization.kotlinx.json.*
-import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.request.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
-import kotlin.test.*
+import io.ktor.http.*
 import io.ktor.server.testing.*
-import rafe95.com.github.plugins.*
+import rafe95.com.github.plugins.configureRouting
+import rafe95.com.github.plugins.configureSerialization
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
-class ApplicationTest {
+class OrderRouteTests {
     @Test
-    fun testRoot() = testApplication {
+    fun testGetOrder() = testApplication {
         application {
             configureRouting()
+            configureSerialization()
         }
-        client.get("/").apply {
-            assertEquals(HttpStatusCode.OK, status)
-            assertEquals("Hello World!", bodyAsText())
-        }
+        val response = client.get("/order/2020-04-06-01")
+        assertEquals(
+            """{"number":"2020-04-06-01","contents":[{"item":"Ham Sandwich","amount":2,"price":5.5},{"item":"Water","amount":1,"price":1.5},{"item":"Beer","amount":3,"price":2.3},{"item":"Cheesecake","amount":1,"price":3.75}]}""",
+            response.bodyAsText()
+        )
+        assertEquals(HttpStatusCode.OK, response.status)
     }
 }
