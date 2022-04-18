434af48e9fd349255af5fe4bb89ed4805de73c43
Rafael Estevez <rafe.estevez@gmail.com>
2022-04-18 00:53:27 -0400

    add: Order models and routing

--- ktor-api/src/main/kotlin/rafe95/com/github/plugins/Routing.kt
+++ ktor-api/src/main/kotlin/rafe95/com/github/plugins/Routing.kt
@@ -3,10 +3,16 @@ package rafe95.com.github.plugins
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 import rafe95.com.github.routes.customerRouting
+import rafe95.com.github.routes.getOrderRoute
+import rafe95.com.github.routes.listOrdersRoute
+import rafe95.com.github.routes.totalizeOrderRoute
 
 fun Application.configureRouting() {
 
     routing {
         customerRouting()
+        listOrdersRoute()
+        getOrderRoute()
+        totalizeOrderRoute()
     }
 }
